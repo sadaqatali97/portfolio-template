0ea3150ed9f11f2fc44355f9a879ea5a9e760f60
sadaqatali97 <sadaqat@iplex.co>
2021-12-22 14:01:05 -0500

    template monster change

--- js/custom.js
+++ js/custom.js
@@ -1,128 +1,149 @@
-/*!
-  * Item: Portfolio
-  * Description: Personal Portfolio Template
-  * Author/Developer: SadaqatCode
-  * Version: v1.0.0
-*/
+/**
+ * /*!
+ * Item: Portfolio
+ * Description: Personal Portfolio Template
+ * Author/Developer: ExifyThemes
+ * Version: v1.0.0
+ *
+ * @format
+ */
 
-
-  //--------------------------------------------------------------------//
+//--------------------------------------------------------------------//
 //-----------------    ISOTOPES    --------------------//
 //--------------------------------------------------------------------//
-  function isotopeMasonry() {
-      $(".isotope-gutter").isotope({
-          itemSelector: '.work__col',
-          percentPosition: true
-      });
-      $(".isotope-no-gutter").isotope({
-          itemSelector: '.work__col',
-          percentPosition: true,
-          masonry: {
-              columnWidth: 1
-          }
-      });
+function isotopeMasonry() {
+  $('.isotope-gutter').isotope({
+    itemSelector: '.work__col',
+    percentPosition: true,
+  });
+  $('.isotope-no-gutter').isotope({
+    itemSelector: '.work__col',
+    percentPosition: true,
+    masonry: {
+      columnWidth: 1,
+    },
+  });
 
-      $(".filter__tab a").on("click", function () {
-          $(".filter__tab a").removeClass("active");
-          $(this).addClass("active");
+  $('.filter__tab a').on('click', function () {
+    $('.filter__tab a').removeClass('active');
+    $(this).addClass('active');
 
-          // portfolio fiter
-          var selector = $(this).attr("data-filter");
-          $(".isotope-gutter").isotope({
-              filter: selector,
-              animationOptions: {
-                  duration: 750,
-                  easing: "linear",
-                  queue: false
-              }
-          });
-          return false;
-      });
-  }
-
+    // portfolio fiter
+    var selector = $(this).attr('data-filter');
+    $('.isotope-gutter').isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false,
+      },
+    });
+    return false;
+  });
+}
 
 //--------------------------------------------------------------------//
 //-----------------    Added bg on HEADER onScroll    --------------------//
 //--------------------------------------------------------------------//
 
-function headerShrink () {
-  $(window).scroll(function() {
+function headerShrink() {
+  $(window).scroll(function () {
     var scrollTop = $(this).scrollTop();
     if (scrollTop > 50) {
-        $("header").addClass("shrink");
+      $('header').addClass('shrink');
     } else {
-      $("header").removeClass("shrink");
+      $('header').removeClass('shrink');
     }
 
     if (scrollTop > 1000) {
-        $(".scroll__top__button").addClass("show");
+      $('.scroll__top__button').addClass('show');
     } else {
-      $(".scroll__top__button").removeClass("show");
+      $('.scroll__top__button').removeClass('show');
     }
   });
 }
 
+//--------------------------------------------------------------------//
+//-----------------    GetFullYeaer JS    --------------------//
+//--------------------------------------------------------------------//
+function getFullYeaer() {
+  var d = new Date();
+  var n = d.getFullYear();
+  document.getElementById('year').innerHTML = n;
+}
 
 //--------------------------------------------------------------------//
 //-----------------    SKILLS TYPING JS    --------------------//
 //--------------------------------------------------------------------//
 
-  const typedTextSpan = document.querySelector(".typed-text");
-  const cursorSpan = document.querySelector(".cursor");
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
 
-  const textArray = ["React.js","Vue.js","Express.js","MongoDB","SQL","PHP", "CI/CD", "AWS","Docker","Kubernetes"];
-  const typingDelay = 30;
-  const erasingDelay = 50;
-  const newTextDelay = 500; // Delay between current and next text
-  let textArrayIndex = 0;
-  let charIndex = 0;
+const textArray = [
+  'React.js',
+  'Vue.js',
+  'Express.js',
+  'MongoDB',
+  'SQL',
+  'PHP',
+  'CI/CD',
+  'AWS',
+  'Docker',
+  'Kubernetes',
+];
+const typingDelay = 30;
+const erasingDelay = 50;
+const newTextDelay = 500; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
 
-  function type() {
-    if (charIndex < textArray[textArrayIndex].length) {
-      if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-      charIndex++;
-      setTimeout(type, typingDelay);
-    } 
-    else {
-      cursorSpan.classList.remove("typing");
-      setTimeout(erase, newTextDelay);
-    }
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains('typing'))
+      cursorSpan.classList.add('typing');
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove('typing');
+    setTimeout(erase, newTextDelay);
   }
+}
 
-  function erase() {
-    if (charIndex > 0) {
-      if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-      typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-      charIndex--;
-      setTimeout(erase, erasingDelay);
-    } 
-    else {
-      cursorSpan.classList.remove("typing");
-      textArrayIndex++;
-      if(textArrayIndex>=textArray.length) textArrayIndex=0;
-      setTimeout(type, typingDelay + 1100);
-    }
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains('typing'))
+      cursorSpan.classList.add('typing');
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove('typing');
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
   }
+}
 
-  document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-    if(textArray.length) setTimeout(type, newTextDelay + 250);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
 //--------------------------------------------------------------------//
 //-----------------    HIDE & SHOW HEADER ON SCROLL    --------------------//
 //--------------------------------------------------------------------//
 
-
-
-function animateHeaderOnScroll(){
+function animateHeaderOnScroll() {
   const body = document.body;
-  const triggerMenu = document.querySelector("header");
-  const scrollUp = "scroll-up";
-  const scrollDown = "scroll-down";
+  const triggerMenu = document.querySelector('header');
+  const scrollUp = 'scroll-up';
+  const scrollDown = 'scroll-down';
   let lastScroll = 0;
 
-
-  window.addEventListener("scroll", () => {
+  window.addEventListener('scroll', () => {
     const currentScroll = window.pageYOffset;
     if (currentScroll <= 0) {
       body.classList.remove(scrollUp);
@@ -146,86 +167,105 @@ function animateHeaderOnScroll(){
   });
 }
 
-
-
-
 //--------------------------------------------------------------------//
 //-----------------    CONTACT FORM SUBMISSION    --------------------//
 //--------------------------------------------------------------------//
 
-function contactForm(){
-	
-	"use strict";
-	
-	$("#submit__btn").on('click', function(){
-		
-		var name 		= $(".contact__form #name").val();
-		var email 		= $(".contact__form #email").val();
-		var message 	= $(".contact__form #message").val();
-		var phone 	= $(".contact__form #phone").val();
-		var success     = $(".contact__form .returnmessage").data('success');
-	
-		$(".contact__form .returnmessage").empty();
-		
+function contactForm() {
+  'use strict';
 
-		if(name===''||email===''||message===''||phone==''){
-      $('div.empty_notice').addClass("d-flex");
-		}
-		else{
+  $('#submit__btn').on('click', function () {
+    var name = $('.contact__form #name').val();
+    var email = $('.contact__form #email').val();
+    var message = $('.contact__form #message').val();
+    var phone = $('.contact__form #phone').val();
+    var success = $('.contact__form .returnmessage').data('success');
+
+    $('.contact__form .returnmessage').empty();
+
+    if (name === '' || email === '' || message === '' || phone == '') {
+      $('div.empty_notice').addClass('d-flex');
+    } else {
       // $(".contact__form .returnmessage").innerText = "dklashhdios";
-      $( ".contact__form .returnmessage" ).first().text("Your message has been received, We will contact you soon.");
-      $('div.empty_notice').removeClass("d-flex");
-			// // Returns successful data submission message when the entered information is stored in database.
-			// $.post("modal/contact.php",{ ajax_name: name, ajax_email: email, ajax_message:message, ajax_phone: phone}, function(data) {
-				
-			// 	$(".contact__form .returnmessage").append(data);//Append returned message to message paragraph
-				
-				
-			// 	if($(".contact__form .returnmessage span.contact_error").length){
-			// 		$(".contact__form .returnmessage").slideDown(500).delay(2000).slideUp(500);		
-			// 	}else{
-			// 		$(".contact__form .returnmessage").append("<span class='contact_success'>"+ success +"</span>");
-			// 		$(".contact__form .returnmessage").slideDown(500).delay(4000).slideUp(500);
-			// 	}
-				
-			// 	if(data===""){
-			// 		$("#contact__form")[0].reset();//To reset form fields on success
-			// 	}
-				
-			// });
-		}
-		return false; 
-	});
+      $('.contact__form .returnmessage')
+        .first()
+        .text('Your message has been received, We will contact you soon.');
+      $('div.empty_notice').removeClass('d-flex');
+      // // Returns successful data submission message when the entered information is stored in database.
+      $.post(
+        'http://localhost/html/send_mail.php',
+        {
+          ajax_name: name,
+          ajax_email: email,
+          ajax_message: message,
+          ajax_phone: phone,
+        },
+        function (data) {
+          console.log('response', data);
+
+          // $(".contact__form .returnmessage").append(data);//Append returned message to message paragraph
+
+          // if($(".contact__form .returnmessage span.contact_error").length){
+          // 	$(".contact__form .returnmessage").slideDown(500).delay(2000).slideUp(500);
+          // }else{
+          // 	$(".contact__form .returnmessage").append("<span class='contact_success'>"+ success +"</span>");
+          // 	$(".contact__form .returnmessage").slideDown(500).delay(4000).slideUp(500);
+          // }
+
+          // if(data===""){
+          // 	$("#contact__form")[0].reset();//To reset form fields on success
+          // }
+        }
+      );
+    }
+    return false;
+  });
 }
 
 //--------------------------------------------------------------------//
 //-----------------    Scroll to Section    --------------------//
 //--------------------------------------------------------------------//
 function scrollToSection() {
-  $(".nav-link[href^='#'], .footer__list a[href^='#'").click(function(e) {
+  $(".nav-link[href^='#'], .footer__list a[href^='#'").click(function (e) {
     e.preventDefault();
-    
-    var position = $($(this).attr("href")).offset().top;
-  
-    $("body, html").animate({
-      scrollTop: position - 30
-    }, 'slow' );
+
+    var position = $($(this).attr('href')).offset().top;
+
+    $('body, html').animate(
+      {
+        scrollTop: position - 30,
+      },
+      'slow'
+    );
   });
 }
 
-// $('html,body').animate({
-//   scrollTop: $('.page-block-glossary').offset().top - HeaderHeight,
-// }, 'slow');
+//--------------------------------------------------------------------//
+//-----------------    Fields Focus JS    --------------------//
+//--------------------------------------------------------------------//
+function fieldsFocus() {
+  $('.form-control').val('');
+
+  $('.form-control').focusout(function () {
+    var text_val = $(this).val();
+    if (text_val === '') {
+      console.log('empty!');
+      $(this).parent().removeClass('input__focus');
+    } else {
+      $(this).parent().addClass('input__focus');
+    }
+  });
+}
 
 //--------------------------------------------------------------------//
 //-----------------    Window ScrollTop    --------------------//
 //--------------------------------------------------------------------//
 function scrollTopBtnFunction() {
   var scroll__top__button = $('.scroll__top__button');
-  
-  scroll__top__button.on('click', function(e) {
+
+  scroll__top__button.on('click', function (e) {
     e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
+    $('html, body').animate({ scrollTop: 0 }, '300');
   });
 }
 
@@ -237,42 +277,41 @@ function skillsCarousel() {
     loop: true,
     nav: true,
     autoplay: true,
-    responsive:{
-        0:{
-            items:1
-        },
-        576:{
-            items: 2,
-        },
-        992:{
-            items:3
-        }
-    }
-})
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
 }
 
 // init functions
-$(document).ready(function() {
+$(document).ready(function () {
   // Add acto class on nav-item
-    $('.nav-item').click(function(){
-        $('.nav-item').removeClass("active");
-        $(this).addClass("active");
-    });
+  $('.nav-item').click(function () {
+    $('.nav-item').removeClass('active');
+    $(this).addClass('active');
+  });
 
-    // Preloader jQuery
-    setTimeout(function(){ 
-      $(".preloader").css("display", "none");          
-     }, 700); 
+  // Preloader jQuery
+  setTimeout(function () {
+    $('.preloader').css('display', 'none');
+  }, 700);
 
-    // Init AOS
-    AOS.init();
+  // Init AOS
+  AOS.init();
 
-    // Close Nav on scroll
-    $('.nav-item a').click(function(){
-      $('.navbar-collapse').removeClass("show");
-      $(".ham").removeClass("active");
-    });
-
+  // Close Nav on scroll
+  $('.nav-item a').click(function () {
+    $('.navbar-collapse').removeClass('show');
+    $('.ham').removeClass('active');
+  });
 
   // Initialize functions
   animateHeaderOnScroll();
@@ -282,6 +321,6 @@ $(document).ready(function() {
   skillsCarousel();
   scrollTopBtnFunction();
   scrollToSection();
+  getFullYeaer();
+  fieldsFocus();
 });
-
-
---
+++ send_mail.php
@@ -0,0 +1,47 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+ $name = $_POST['ajax_name'];
+ $email = $_POST['ajax_email'];
+ $phone = $_POST['ajax_phone'];
+ $message = $_POST['ajax_message'];
+ if (!empty($name)) {
+  $to = "sadaqat@iplex.co";
+  $subject = "Portfolio Email";
+  
+  $message = "
+  <html>
+  <head>
+  <title>HTML email</title>
+  </head>
+  <body>
+  <p>This email contains HTML Tags!</p>
+  <table>
+  <tr>
+  <th>Firstname</th>
+  <th>Email</th>
+  <th>Phone</th>
+  <th>Message</th>
+  </tr>
+  <tr>
+  <td>".$name."</td>
+  <td>".$email."</td>
+  <td>".$phone."</td>
+  <td>".$message."</td>
+  </tr>
+  </table>
+  </body>
+  </html>
+  ";
+  
+  // Always set content-type when sending HTML email
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+  
+  // More headers
+  $headers .= 'From: '.$email . "\r\n";
+ $response =  mail($to,$subject,$message,$headers);
+ echo $response;
+ }
+
+?>
